ec413196d7ea8e507847e9be9fade5d52dd35f77
Bratapfel2000 <43928387+Bratapfel2000@users.noreply.github.com>
2020-06-01 17:24:51 +0200

    Add files via upload

---
+++ digit_recognition/digits_1.py
@@ -0,0 +1,103 @@
+"""
+================================
+Recognizing hand-written digits
+================================
+
+An example showing how the scikit-learn can be used to recognize images of
+hand-written digits.
+
+This example is commented in the
+:ref:`tutorial section of the user manual <introduction>`.
+
+"""
+print(__doc__)
+
+# Author: Gael Varoquaux <gael dot varoquaux at normalesup dot org>
+# License: BSD 3 clause
+
+# Standard scientific Python imports
+import matplotlib.pyplot as plt
+
+# Import datasets, classifiers and performance metrics
+from sklearn import datasets, svm, metrics
+from sklearn.model_selection import train_test_split
+
+# The digits dataset
+digits = datasets.load_digits()
+#.savefig('plot_digits.png')
+
+
+a1 = clf = svm.SVC(gamma=0.001, C=100.)
+
+a2 = clf.fit(digits.data[:-10], digits.target[:-10])
+print(a2)
+
+a3 = clf.predict(digits.data[-10:])
+
+print(a3)
+
+#x,y = digits.data[:-1], digits.target[:-1]
+
+plt.imshow(digits.images[-1], cmap=plt.cm.gray_r, interpolation="nearest")
+
+#data = plt.imread(filename)
+
+#matplotlib.pyplot.imread
+# The data that we are interested in is made of 8x8 images of digits, let's
+# have a look at the first 4 images, stored in the `images` attribute of the
+# dataset.  If we were working from image files, we could load them using
+# matplotlib.pyplot.imread.  Note that each image must have the same size. For these
+# images, we know which digit they represent: it is given in the 'target' of
+# the dataset.
+_, axes = plt.subplots(2, 4)
+images_and_labels = list(zip(digits.images, digits.target))
+
+
+for ax, (image, label) in zip(axes[0, :], images_and_labels[:4]):
+    #print(image)
+    ax.set_axis_off()
+    ax.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+    ax.set_title('Training: %i' % label)
+
+# To apply a classifier on this data, we need to flatten the image, to
+# turn the data in a (samples, feature) matrix:
+n_samples = len(digits.images)
+
+data = digits.images.reshape((n_samples, -1))
+
+
+
+#print(500*"  ")
+#print(">>",ax)
+#print(500*"  ")
+
+
+
+# Create a classifier: a support vector classifier
+classifier = svm.SVC(gamma=0.001)
+
+# Split data into train and test subsets
+X_train, X_test, y_train, y_test = train_test_split(
+    data, digits.target, test_size=0.5, shuffle=True)
+
+# We learn the digits on the first half of the digits
+classifier.fit(X_train, y_train)
+
+# Now predict the value of the digit on the second half:
+predicted = classifier.predict(X_test)
+
+
+
+images_and_predictions = list(zip(digits.images[n_samples // 2:], predicted))
+for ax, (image, prediction) in zip(axes[1, :], images_and_predictions[:4]):
+    ax.set_axis_off()
+    ax.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+    ax.set_title('Prediction: %i' % prediction)
+
+print("Classification report for classifier %s:\n%s\n"
+      % (classifier, metrics.classification_report(y_test, predicted)))
+#disp = metrics.plot_confusion_matrix(classifier, X_test, y_test)
+#disp.figure_.suptitle("Confusion Matrix")
+#print("Confusion matrix:\n%s" % disp.confusion_matrix)
+#plt.savefig('plot.png')
+plt.show()
